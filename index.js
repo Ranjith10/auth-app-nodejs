aa1ae70c7f54fe6dee59820dc44db43ca01e0b99
Ranjith kumar <ranjit4072@gmail.com>
2020-06-22 19:00:07 +0530

    Added loggers for API

--- index.js
+++ index.js
@@ -2,17 +2,21 @@ require('dotenv').config();
 
 const express = require('express')
 const bodyParser = require('body-parser')
-const router = express.Router()
+const fs = require('fs')
+const path = require('path')
+const morgan = require('morgan')
 const login = require('./routes/login.js')
 
 //Express app
 const app = express()
 
 const port = process.env.port || 5000
+const accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' })
 
-//Bodyparser 
+//Middleware 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(morgan('combined', { stream: accessLogStream }))
 
 //CORS handling
 app.use((req, res, next) => {
